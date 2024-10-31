284b470bf8065754793af190d60777be55e50d90
Pablo Benito Cia <pablo.benito.cia@gmail.com>
2024-10-31 20:35:23 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# The Lorenz system
+
+https://pablo-benito.github.io/lorenz
+
+Lorenz Attractor and the butterfly effect
---
+++ code.js
@@ -0,0 +1,100 @@
+let zoom = 500; 
+let lastMouseX = 0;
+let lastMouseY = 0;
+let camX = 1;
+let camY = 0;
+
+class Trajectory {
+  a = 10;
+  b = 30;
+  c = 8.0 / 3.0;
+  dt = 0.01;
+  MAX_POINTS = 2000
+
+  constructor(x, y, z, hue) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.hue = hue;
+    this.points = [];
+  }
+
+  draw() {
+    for (let i = 0; i < 3; i++) {
+      let dx = (this.a * (this.y - this.x)) * this.dt;
+      let dy = (this.x * (this.b - this.z) - this.y) * this.dt;
+      let dz = (this.x * this.y - this.c * this.z) * this.dt;
+      this.x += dx;
+      this.y += dy;
+      this.z += dz;
+
+      this.points.push({
+        pos: createVector(this.x, this.y, this.z),
+        hue: this.hue
+      });
+    }
+
+    push()
+
+      let hu = this.hue;      
+      stroke(hu, 255, 255, 100); 
+      setAttributes('antialias', true);
+
+      beginShape();
+
+        for (let i = 1; i < this.points.length; i++) {
+          let v1 = this.points[i - 1].pos;  
+          let v2 = this.points[i].pos;  
+          vertex(v1.x * 10, v1.y * 10, v1.z * 10); 
+          vertex(v2.x * 10, v2.y * 10, v2.z * 10);
+        }
+
+      endShape()
+      let pp = this.points[this.points.length - 1].pos;
+      translate(pp.x * 10, pp.y * 10, pp.z * 10); 
+
+      sphere(5);
+
+    pop();
+
+    if (this.points.length > this.MAX_POINTS) {
+      this.points = this.points.slice(this.points.length - this.MAX_POINTS, this.points.length)
+    }
+
+  }
+}
+
+
+function setup() {
+  var canvas = createCanvas(windowWidth, 600, WEBGL);
+  canvas.parent("p5jsContainer");
+
+  colorMode(HSB, 360, 100, 100, 100);
+  strokeWeight(1.85);
+  frameRate(60)
+
+  t1_color = random(0, 360)
+  t2_color = (t1_color + 120) % 360
+  t1 = new Trajectory(0.01, 0, 0, t1_color)
+  t2 = new Trajectory(0.01 + 0.2*random(-1, 1), 0, 0, t2_color)
+  noFill();
+
+}
+
+function draw() {
+
+  background(230, 50, 15);
+
+  translate(camX, camY, -zoom);
+
+  let angle = frameCount * 0.01;
+  rotateX(angle);
+  rotateY(angle);
+  rotateZ(angle);
+  
+  t1.draw()
+  t2.draw()
+
+  orbitControl();
+
+}
